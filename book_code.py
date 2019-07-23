d3b027edfddead1ca92ce7e518c16feb9f205d62
Christopher Souza <christopher.souza@ctec.ufal.br>
2019-07-23 12:07:23 -0300

    Inserting chapter 18

--- book_code.py
+++ book_code.py
@@ -1,3 +1,5 @@
+import random
+
 def int_to_time(seconds):
     time = Time()
     minutes, time.second = divmod(seconds, 60)
@@ -56,8 +58,65 @@ class Time:
         return self.__add__(other)
 
     
-class Point():
+class Point:
     
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+    
+
+class Card:
+    """Represents a standard playing card."""
+
+    suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7',
+                  '8', '9', '10', 'Jack', 'Queen', 'King']
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+        
+    def __str__(self):
+        return '%s of %s' % (Card.rank_names[self.rank],
+                             Card.suit_names[self.suit])
+    
+    def __lt__(self, other):
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return t1 < t2
+    
+    
+class Deck:
+    
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+                
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+    
+    def pop_card(self):
+        return self.cards.pop()
+    
+    def add_card(self, card):
+        self.cards.append(card)
+        
+    def shuffle(self):
+        random.shuffle(self.cards)
+        
+    def move_cards(self, hand, num):
+        for i in range(num):
+            hand.add_card(self.pop_card())
+        
+class Hand(Deck):
+    """Represents a hand of playing cards."""
+    
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
